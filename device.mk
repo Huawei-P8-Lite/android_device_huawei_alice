b9f02db1cddf35ec5ec635dd5f26bdd21d83d10b
Thespartann <danilastefan69@yahoo.ro>
2021-04-29 14:17:36 +0300

    alice: Remove VNDK
    * This device doesn't have treble
    
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- device.mk
+++ device.mk
@@ -261,10 +261,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # Wi-Fi
 PRODUCT_PACKAGES += \
     libwpa_client \
